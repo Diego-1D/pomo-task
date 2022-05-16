9a6f59ecd2c8edff693c53e837e9b4a2f02d088c
Diego-1D <diego1d_fernandes@outlook.com>
2022-05-15 21:21:23 -0300

    Implementação do modal

--- src/components/ToDoList/index.tsx
+++ src/components/ToDoList/index.tsx
@@ -1,27 +1,57 @@
 import { NewTask } from '../../type/tasks'
 import styles from './todolist.module.scss'
 import Cards from './Cards'
+import React, { useState } from 'react'
+import Form from '../Form'
+import Button from '../Button'
 
 interface Props {
     tasks: NewTask[],
     selectTask: (selectedTask: NewTask) => void
+    setTasks: React.Dispatch<React.SetStateAction<NewTask[]>>
 }
 
-const index = ({ tasks, selectTask }: Props) => {
+const ToDoList = ({ tasks, selectTask, setTasks }: Props) => {
+
+    console.log('Tarefas', tasks)
+
+    const [openModal, setOpenModal] = useState(false)
+
+    const toggle = () => {
+        setOpenModal(!openModal)
+    }
+
     return (
-        <div className={styles.todoList}>
-            <h2>Tarefas do dia:</h2>
+        <div className={styles.todolist}>
+            <div className={styles.header}>
+                <h2>Tarefas do dia:</h2>
+                <Button onClick={toggle}>
+                    + tarefa
+                </Button>
+                {
+                    openModal && <Form setTasks={setTasks} onClose={toggle} />
+                }
+            </div>
             {
-                tasks.map((item) => (
-                    <Cards
-                        selectTask={selectTask}
-                        key={item.id}
-                        {...item}
-                    />
-                ))
+                tasks.length === 0 &&
+                <div className={styles.message}>
+                    <label>Não há nenhuma tarefa</label>
+                </div>
             }
+            <>
+                {
+                    tasks.map((item) => (
+                        <Cards
+                            selectTask={selectTask}
+                            key={item.id}
+                            {...item}
+                        />
+                    ))
+                }
+
+            </>
         </div>
     )
 }
 
-export default index
+export default ToDoList
